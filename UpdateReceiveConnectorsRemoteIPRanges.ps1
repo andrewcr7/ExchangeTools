a4b5da4b380c6699e4f4f33d61fd556d71dc4a2b
andrewcr7 <andrew.riley@gmx.com>
2016-09-01 14:35:36 -0400

    Add files via upload

---
+++ UpdateReceiveConnectorsRemoteIPRanges.ps1
@@ -0,0 +1,52 @@
+﻿add-pssnapin Microsoft.Exchange.Management.PowerShell.E2010
+
+Function UpdateReceiveConnectorsRemoteIPRanges {
+
+    Param(
+        [Parameter(Mandatory=$True,Position=1)]
+            $receiveConnectorIDs,
+	
+        [Parameter(Mandatory=$False)]
+            [string[]]$additionalIPs,
+
+        [Parameter(Mandatory=$False)]
+            [string]$outputPath
+    )
+
+    $receiveConnectorObjects = @()
+
+    foreach ($r in $receiveConnectorIDs) {
+            $receiveConnectorObjects += Get-ReceiveConnector -Identity $r
+    }
+
+    if ($outputPath -eq $null) {
+        $outputPath = ".\"
+    }
+
+    foreach ($r in $receiveConnectorObjects) {
+        $FilePath = ($outputPath + $r.server + "-" + $r.Name + "-" + (Get-Date -Format yyyyMMdd\THHmmss) + ".xml")
+        Export-Clixml -InputObject $r.RemoteIPRanges -Path $FilePath
+    }
+    
+    foreach ($ip in $additionalIPs) {
+
+        if ($ip -eq "") { 
+            continue 
+        }
+        else {           
+            foreach ($r in $receiveConnectorObjects) {
+                if ($r.RemoteIPRanges.Contains($ip)) {
+                    Write-Host "IP Address " $ip " already exists in connector $r.Identity"
+                }
+                else {
+                    $r.RemoteIPRanges += $ip
+                }
+                
+            }
+        }
+    }
+
+    foreach ($r in $receiveConnectorObjects) {
+        Set-ReceiveConnector $r.Identity -RemoteIPRanges $r.RemoteIPRanges -WhatIf
+    }
+}
